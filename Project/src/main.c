3596302b33e914dc424ff26ec3d404351a05b920
Superdose <72471357+Superdose@users.noreply.github.com>
2021-12-07 12:15:25 +0100

    Created basic C-Project structure.
    Added main.c
    Added a function to read user input dynamically during runtime.

---
+++ Project/src/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
+
+/**
+ * Funktion um Benutzereingaben während der Ausführung dynamisch einzulesen
+ */
+char * getline(void) {
+    char * line = malloc(100), * linep = line;
+    size_t lenmax = 100, len = lenmax;
+    int c;
+
+    if(line == NULL)
+        return NULL;
+
+    for(;;) {
+        c = fgetc(stdin);
+        if(c == EOF)
+            break;
+
+        if(--len == 0) {
+            len = lenmax;
+            char * linen = realloc(linep, lenmax *= 2);
+
+            if(linen == NULL) {
+                free(linep);
+                return NULL;
+            }
+            line = linen + (line - linep);
+            linep = linen;
+        }
+
+        if((*line++ = c) == '\n')
+            break;
+    }
+    *line = '\0';
+    return linep;
+}
