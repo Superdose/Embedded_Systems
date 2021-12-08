42a5c3641d99d3eaa284cc1ed40c6dc86316c519
brunofight <55789698+brunofight@users.noreply.github.com>
2021-12-08 13:41:54 +0100

    Update rfid.c

--- Project/src/rfid.c
+++ Project/src/rfid.c
@@ -3,7 +3,12 @@
 /* Assumes a pigpio handle was already setup */
 void rfid_write(char addr, char val)
 {
-    spi_write();
+    bcm2835_spi_transfer();		
+}
+
+char rfid_read(char addr)
+{
+    return bcm2835_spi_transfer(addr);	
 }
 
 void rfid_reset()
@@ -11,9 +16,22 @@ void rfid_reset()
 
 }
 
+void rfid_antenna_on() 
+{
+    
+}
+
 int rfid_init()
 {
+    
+    bcm2835_spi_begin();
+    /* heißt wenn CS0 auf low, dann ist CS0 aktive */
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, 0);
+    /* 1.653125 MHz Clock */
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_128);
 
+    /* CS0 aktivieren */
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
 
     rfid_reset();
     rfid_write(REG_TMODE, 0x8D);
