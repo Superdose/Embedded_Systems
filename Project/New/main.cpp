d28d941d92efe5b91e73bd5c434ef50eb8b8cfad
jbkze <jonas.kunze@yahoo.de>
2022-01-25 09:58:15 +0100

    Update main.cpp

--- Project/New/main.cpp
+++ Project/New/main.cpp
@@ -2,39 +2,55 @@
 #include <stdio.h>
 #include "MFRC522.h"
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+
 
 MFRC522 mfrc;
 MFRC522::MIFARE_Key key;
 
-byte sector = 1;
-byte blockAddr = 4;
+// Each sector consists of 4 blocks, i.e. 3 data blocks and one sector trailer
+// Sector 0 has Blocks 0-3 :  0 (manufacturer data), 1 (data),  2 (data),  3 (sector trailer)
+// Sector 1 has Blocks 4-7 :  4 (data),              5 (data),  6 (data),  7 (sector trailer)
+// Sector 2 has Blocks 8-11:  8 (data),              9 (data), 10 (data), 11 (sector trailer)
+// and so on...
+byte sector                 = 1;  // The sector we want to write data to
+byte nameBlock              = sector * 4 + 0; // Name of the card owner
+byte keyBlock               = sector * 4 + 1; // Secret KeySecurity
+//byte ...                  = sector * 4 + 2; // Not used
+byte trailerBlock           = sector * 4 + 3; // This is the sector trailer block in sector 1
 
-byte secretKey[]    = {
-        0x01, 0x03, 0x03, 0x07, 
-        0x01, 0x03, 0x03, 0x07, 
-        0xff, 0xee, 0xdd, 0xcc, 
-        0xdc, 0x3d, 0x4d, 0xc3  
-    };
-byte purge[]    = {
-        0x00, 0x00, 0x00, 0x00, 
-        0x00, 0x00, 0x00, 0x00, 
-        0x00, 0x00, 0x00, 0x00, 
-        0x00, 0x00, 0x00, 0x00  
-    };
-byte trailerBlock = 7;
+
+// The array we read the data into (16 bytes for data, 2 for CRC)
 byte buffer[18];
 byte size = sizeof(buffer);
 
-void toHexString(byte *buffer, byte bufferSize, char *output) {
-  char lookup[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-    for (int i = 0; i < bufferSize; i++) {
-        output[2*i  ] = lookup[(buffer[i]>>4)&0xF];
-        output[2*i+1] = lookup[buffer[i] & 0xF];
+// Secret Key that is stored on the card
+//byte[16] secretKey = "v3ry53cr37k3y123"
+//byte[16] flushKey = "0000000000000000"
+
+
+void nicePrint(const string &output)
+{
+    cout << endl << endl;
+
+    const int width = 44;
+    const char c = '*';
+    int stringLength = static_cast<int>(output.length());
+
+    string horizontal_line(width, c);
+
+    cout << horizontal_line << endl;
+    if(stringLength % 2 == 1) {
+        cout << c << setw(width / 2 - 2 - stringLength / 2) << "" << left << output << setw(width / 2 - stringLength / 2) << right << c << endl;
+    } else{
+        cout << c << setw(width / 2 - 2 - stringLength / 2) << "" << left << output << setw(width / 2 - stringLength / 2 + 1) << right << c << endl;
     }
-    output[2*bufferSize] = 0;
+    cout << horizontal_line << endl << endl;
 }
 
-void read()
+
+string getUID() 
 {
   while(1){
     // Look for a card
@@ -43,7 +59,32 @@ void read()
 
     if( !mfrc.PICC_ReadCardSerial())
       continue;
+    
+    // --- Read UID ---
+    stringstream uid;
+    
+    for(byte i = 0; i < mfrc.uid.size; ++i){
+      uid << std::uppercase << std::setfill('0') << std::setw(2) << hex << (int) mfrc.uid.uidByte[i] << " ";
+    }
+    
+    return uid.str();        
+  }
+}
 
+
+string read(byte sector, byte block)
+{
+  while(1){
+    // Look for a card
+    if(!mfrc.PICC_IsNewCardPresent())
+      continue;
+
+    if( !mfrc.PICC_ReadCardSerial())
+      continue;
+    
+
+    // Calculate block number of sector trailer
+    byte trailerBlock = sector * 4 + 3;
     
     MFRC522::StatusCode status;
 
@@ -52,24 +93,30 @@ void read()
     if (status != MFRC522::STATUS_OK) {
         cout << "MIFARE_Authenticate() failed: ";
         cout << mfrc.GetStatusCodeName(status);
+        continue;
     }
     
-    status = (MFRC522::StatusCode) mfrc.MIFARE_Read(blockAddr, buffer, &size);
+    // --- Read Data ---        
+    status = (MFRC522::StatusCode) mfrc.MIFARE_Read(block, buffer, &size);
+  
+    string data;    
     
-    char out[64];
-    toHexString(buffer, 16, out);
-    cout << out << "\n";
+    for(int i=0; i< 16; i++)
+    {
+      data.push_back((char) (int) buffer[i]);
+    }
     
     // Halt PICC
     mfrc.PICC_HaltA();
     // Stop encryption on PCD
     mfrc.PCD_StopCrypto1();
-    break;
+    
+    return data;
     
   }
 }
 
-void write(byte value[])
+void write(byte sector, byte block, byte value[], byte size)
 {
   while(1)
   {
@@ -79,9 +126,12 @@ void write(byte value[])
     if( !mfrc.PICC_ReadCardSerial())
       continue;
 
-        
+  
+    // Calculate block number of sector trailer
+    byte trailerBlock = sector * 4 + 3;
+    
     MFRC522::StatusCode status;
-
+  
     status = (MFRC522::StatusCode) mfrc.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc.uid));
 
     if (status != MFRC522::STATUS_OK) {
@@ -89,16 +139,17 @@ void write(byte value[])
         cout << mfrc.GetStatusCodeName(status);
     }
 
-    status = (MFRC522::StatusCode) mfrc.MIFARE_Write(blockAddr, value, 16);
+    status = (MFRC522::StatusCode) mfrc.MIFARE_Write(block, value, size);
 
     if (status != MFRC522::STATUS_OK) {
         cout << "MIFARE_Write() failed: ";
         cout << mfrc.GetStatusCodeName(status);
+        
+        continue;
     } else {
-      cout << "Write Success\n";
+      cout << "Write Success" << endl;
     }
 
-    
     // Halt PICC
     mfrc.PICC_HaltA();
     // Stop encryption on PCD
@@ -113,31 +164,74 @@ void write(byte value[])
 int main()
 {
   mfrc.PCD_Init();
+  // The default key is 0xFFFFFFFFFFFF
   for (byte i = 0; i < 6; i++) {
         key.keyByte[i] = 0xFF;
     }
 
+  
   while(1)
   {
-    cout << "Card-Tool\n";
-    cout << "1 - register new card\n";
-    cout << "2 - delete card\n";
-    cout << "3 - print card information\n";
-
+    nicePrint("Card Registration Service");
+    cout << "1 - Register new card\n";
+    cout << "2 - Delete card\n";
+    cout << "3 - Print card information\n";
+    
+    
     int choice;
+    cout << endl << "Your choice: ";
     cin >> choice;
-
+    
+    cin.ignore();
+    
     switch (choice)
     {
     case 1:
-      write(secretKey);
-      break;
+      {
+        std::string name;
+        cout << "Please enter the name of the card recipient: ";
+        std::getline(cin, name);
+        
+        byte nameArr[16] = {(int) ' '};  // array filled with whitespaces
+        
+        for(int i = 0; i < min<size_t>(16, name.size()); i++) {
+          nameArr[i] = (int) name[i];
+        }
+        
+        cout << "Waiting for RFID Chip... " << std::flush;
+        
+        write(sector, nameBlock, nameArr, 16);
+        //write(sector, keyBlock, secretKey, 16);
+     
+        break;
+     }
+     
     case 2:
-      write(purge);
-      break;
+      { 
+        byte nameArr[16] = {(int) ' '};  // array filled with whitespaces
+               
+        write(sector, nameBlock, nameArr, 16);
+        //write(sector, keyBlock, flushKey, 16);
+     
+        break;
+     }
+  
     case 3:
-      read();
-      break;    
+      {    
+        string uid = getUID();
+        string name = read(sector, nameBlock);
+        //string key = read(sector, keyBlock);
+        
+        cout << "Waiting for RFID Chip... " << std::flush;
+        
+        cout << endl << endl;                
+        cout << "--- Card Information ---" << endl << endl;
+        cout << "Card ID:    " << uid << endl;
+        cout << "Card Owner: " << name << endl;
+        //cout << "Secret Key: " << key << endl;
+        break;
+      }      
+      
     default:
       break;
     }
